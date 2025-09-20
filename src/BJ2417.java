17040c349971ca0892a15d9042b4d5787d574887
Shihyun Han <devowl99@gmail.com>
2025-09-20 19:28:43 +0900

    250920

---
+++ src/BJ2417.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class BJ2417 {
+	static BigInteger N;
+		
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = new BigInteger(br.readLine());
+		
+		long q = 0;
+		long l = 0;
+		long r = N.longValue();
+		
+		while (l<=r) {
+			long mid = (l+r)/2;
+			
+            BigInteger midBig = BigInteger.valueOf(mid);    
+			
+			if (midBig.multiply(midBig).compareTo(N) >= 0) {
+				q = mid;
+				r = mid-1;
+			}
+			else {
+				l = mid+1;
+			}
+		}
+		
+		System.out.println(q);
+	}
+}
