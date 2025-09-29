f5bab7c2175d709819f241f1abc1108a1567466c
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2025-09-29 17:29:11 +0900

    250929

---
+++ src/BJ9012.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ9012 {
+	static int T;
+	static Queue<String> q;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		T = Integer.parseInt(br.readLine());
+		
+		for (int t=0; t<T; t++) {
+			q = new ArrayDeque<>();
+			
+		}
+	}
+}
