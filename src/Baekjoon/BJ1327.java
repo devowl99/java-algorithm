2122cd3762dad079ce08eedad97e17a88163b31a
Shihyun Han <devowl99@gmail.com>
2025-10-21 21:55:32 +0900

    251021

---
+++ src/Baekjoon/BJ1327.java
@@ -0,0 +1,11 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ1327 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Baekjoon/BJ26069.java
@@ -0,0 +1,36 @@
+package Baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ26069 {
+    static int N;
+    static Set<String> dancers;
+    static String p1;
+    static String p2;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        dancers = new HashSet<>();
+        dancers.add("ChongChong");
+
+        StringTokenizer st;
+        for (int n=0; n<N; n++){
+            st = new StringTokenizer(br.readLine());
+
+            p1 = st.nextToken();
+            p2 = st.nextToken();
+
+            if (dancers.contains(p1)) {
+                dancers.add(p2);
+            }
+            else if (dancers.contains(p2)) {
+                dancers.add(p1);
+            }
+        }
+
+        System.out.println(dancers.size());
+    }
+}
