6bb886c8bc3a738d3ea26635cedcff0e3be4bb1e
Shihyun Han <152502583+devowl99@users.noreply.github.com>
2025-10-29 16:36:23 +0900

    251029

---
+++ src/Inflearn/INF1.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class INF1 {
+    static String str;
+    static String c;
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        str = br.readLine();
+        String lowStr = str.toLowerCase();
+
+        c = br.readLine();
+        char lowC = c.toLowerCase().charAt(0);
+
+        count = 0;
+        for (int i=0; i<str.length(); i++){
+            if (lowStr.charAt(i) == lowC) count++;
+        }
+
+        System.out.println(count);
+    }
+}
